387b496058dad5657e23c16ecce694dcc460effa
joaobrunoah <joaobrunoah@gmail.com>
2023-05-03 16:33:52 -0300

    Fixing CoinFlip files

--- 3. CoinFlip/ICoinFlip.sol
+++ 3. CoinFlip/ICoinFlip.sol
@@ -1,7 +1,9 @@
+pragma solidity ^0.8.0;
+
 interface ICoinFlip {
     /*
      * This function is defined in https://ethernaut.openzeppelin.com/level/0xA62fE5344FE62AdC1F356447B669E9E6D10abaaF
      * @param _guess contains the guessing of the next result of the coin flip (true or false)
      */
-    function flip(bool _guess) public returns (bool);
+    function flip(bool _guess) external returns (bool);
 }
